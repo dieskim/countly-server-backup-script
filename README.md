f9f38dda545f3a354730db6a10d340585095c623
DieSkim <dieskim@julula.com>
2015-05-13 19:19:12 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Count.ly Server Backup Script
+
+Countly is an innovative, real-time, open source mobile analytics application. It collects data from mobile phones, and visualizes this information to analyze mobile application usage and end-user behavior. There are two parts of Countly: the server that collects and analyzes data, and mobile SDK that sends this data (for iOS & Android).
+
+Countly:
+
+- [Countly (Countly)](https://count.ly)
+
+Countly Server;
+
+- [Countly Server (countly-server)](https://github.com/Countly/countly-server)
+
+Titanium Countly Messaging Modules
+
+- [Countly Titanium Android Messaging Module](https://github.com/dieskim/Titanium-Count.ly-Android-Messaging)
+- [Countly Titanium iOS Messaging Module](https://github.com/dieskim/Titanium-Count.ly-Messaging)
+
+Other Countly SDK repositories;
+
+- [Countly Android SDK (countly-sdk-android)](https://github.com/Countly/countly-sdk-android)
+- [Countly iOS SDK (countly-sdk-ios)](https://github.com/Countly/countly-sdk-ios)
+
+Countly SDK Guides;
+- [Countly Android Messaging Guide](http://resources.count.ly/v1.0/docs/countly-push-for-android)
+- [Countly iOS Messaging Guide](http://resources.count.ly/v1.0/docs/countly-push-for-ios)
+
+Countly Backup Guide;
+- [Countly Backing up Countly server Guide](http://resources.count.ly/v1.0/docs/backing-up-countly-server)
+
+# This is a Linux Backup Script to Backup all the nessasary Count.ly Settings and Databases of your Count.ly Server
+# It supports both Local and Remote Backups as well as backup Rotation
+# Please note that this is still a beta script
+# Any pull requests and suggestion welcome!
+# Author: Dieskim
+
+## Installation
+
+1. Log in to you server via SSH as root
+
+2. Create a folder where you would like to place the script and move into the folder
+
+``mkdir countly-backup-script``
+``cd countly-backup-script``
+
+3. wget the backup script
+
+``wget countly-backup.sh``
+
+4. Set the script to be executable
+
+``chmod a+x countly-backup.sh``
+
+5. For remote backups set up [Linux SSH KEY Password Free Automatic Config Authentication](http://kiteplans.info/2015/05/13/how-to-linux-ssh-key-password-free-automatic-config-authentication-backup-sftp-scp/)
+
+6. Edit all the setting in the top of the script.
+
+7. Test the script via
+``sh -x countly-backup.sh 2>&1 | tee debug.txt``
+
+7. Add Script to Crontab to run daily
+
+## Author
+
+Author: Dieskim
+
+## License
+MIT
---
+++ countly-backup.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+## RUN USING 'sh -x countly-backup.sh 2>&1 | tee debug.txt' to get DEBUG OUTPUT AND LOG
+
+##############################################################################################################################
+##                                          PLEASE SET ALL THE OPTIONS BELOW                                                ##
+##                      PLEASE SET UP SSH KEYS WITH CONFIG BEFORE ATTEMPTING REMOTE BACKUP                                  ##
+##  http://kiteplans.info/2015/05/13/how-to-linux-ssh-key-password-free-automatic-config-authentication-backup-sftp-scp/
+
+## SET REMOTE BACKUP TRUE OR FALSE - DEFAULTS TO LOCALBACKUP
+REMOTEBACKUP=true
+
+## SET REMOTESERVER INFO IF USING REMOTE BACKUP - REMEMBER TO FIRST ENABLE SSH KEYS AS IN THE GUIDE ABOVE
+REMOTESERVER=backup-server-nickname
+
+## SET REMOVEDEST
+REMOTEDEST=/home/backup-user/countly-backup
+
+## SET LOCAL DEST - SCRIPT DEFAULTS TO MOVING TAR TO LOCAL DESTINATION
+LOCALDEST=~/countly-backup
+
+## ROTATE BACKUP TRUE OR FALSE
+ROTATEBACKUP=true
+
+## SET KEEPDAYS IF USING ROTATEBACKUP - X AMOUNT OF DAYS BEFORE ROTATING
+KEEPDAYS=7
+
+## SET COUNLY BACKUP SCRIPT ROOT - BACKUP LOG CREATED HERE
+BACKUPROOT=~/countly-backup
+
+## SET BACKUP DIR/TAR NAME
+BACKUPDIRVAR=hostname-countly-backup
+
+## SET COUNTLY LOCATION DIR
+COUNTLYDIR=/usr/local/countly
+
+##############################################################################################################################
+##                              DO NOT EDIT FROM HERE - EXCEPT IF YOU KNOW WHAT YOU ARE DOING                               ##
+
+## SET DATE 
+DATE=`date +%Y-%m-%d`
+
+## START IF ROTATEBACKUP
+if $ROTATEBACKUP
+then
+	BACKUPDIR=$BACKUPDIRVAR-$DATE
+else
+	BACKUPDIR=$BACKUPDIRVAR
+fi
+## END IF ROTATEBACKUP
+
+## CREATE BACKUPDIR
+mkdir $BACKUPROOT
+cd $BACKUPROOT
+mkdir $BACKUPDIR
+
+## CD TO BACKUPDIR
+cd $BACKUPDIR
+
+## BACKUP COUNTLY DB
+mongodump --db countly
+mongodump --db countly_drill
+
+## COPY COUNTLY CONFIG FILES
+cp --parents $COUNTLYDIR/frontend/express/config.js .
+cp --parents $COUNTLYDIR/frontend/express/public/javascripts/countly/countly.config.js .
+cp --parents $COUNTLYDIR/api/config.js .
+cp --parents $COUNTLYDIR/plugins/drill/config.js .
+
+## COPY COUNTLY IMAGES AND CERTIFICATES
+cp -r $COUNTLYDIR/frontend/express/public/appimages .
+cp -r $COUNTLYDIR/frontend/express/public/userimages .
+cp -r $COUNTLYDIR/frontend/express/certificates .
+
+## MOVE UP A DIR
+cd ..
+
+## TAR THE WHOLE BACKUP DIR AND REMOVE DIR IF SUCCESSFUL
+tar -cf $BACKUPDIR.tar $BACKUPDIR && rm -R $BACKUPDIR
+
+## START IF REMOTEBACKUP ELSE LOCALBACKUP
+if $REMOTEBACKUP;
+then
+## SEND TAR TO REMOTE SERVER
+sftp $REMOTESERVER <<_EOF_
+    put $BACKUPDIR.tar $REMOTEDEST/$BACKUPDIR.tar
+    bye
+_EOF_
+
+    ## START IF ROTATEBACKUP REMOVE OLD
+    if $ROTATEBACKUP
+    then
+ssh -T $REMOTESERVER <<_EOF_
+    find $REMOTEDEST/$BACKUPDIRVAR-* -mtime +$KEEPDAYS -exec rm -v {} \; | tee -a $REMOTEDEST/countly-backup.log
+    exit
+_EOF_
+    fi
+    ## END IF ROTATEBACKUP REMOVE OLD
+
+    ## REMOVE LOCAL TAR
+    rm -v $BACKUPDIR.tar | tee -a $BACKUPROOT/countly-backup.log
+else
+    ## MOVE TAR TO LOCALDEST
+    mv -v $BACKUPDIR.tar $LOCALDEST | tee -a $BACKUPROOT/countly-backup.log
+
+    ## START IF ROTATEBACKUP REMOVE OLD
+    if $ROTATEBACKUP
+    then
+        find $LOCALDEST/$BACKUPDIRVAR-* -mtime +$KEEPDAYS -exec rm -v {} \; | tee -a $BACKUPROOT/countly-backup.log
+    fi
+    ## END IF ROTATEBACKUP REMOVE OLD
+fi
+## END IF REMOTEBACKUP ELSE LOCALBACKUP
